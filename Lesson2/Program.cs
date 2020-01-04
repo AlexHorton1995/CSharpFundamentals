36843688a17ba610f8038d3e05d6520978a1a8e0
Alex Horton <alex.d.horton@hotmail.com>
2020-01-04 17:12:25 -0600

    Baseline Code update

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Lesson2
 {
+    /// <summary>
+    /// Lesson 2: Implicit vs Strong Types
+    /// 
+    /// Let's learn the differences between implicit versus strong types of variables.
+    /// </summary>
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1.  IMPLICIT VARIABLE
+
+            //2.  STRONG VARIABLE
+
+            //3.  WHEN WOULD YOU USE ONE OVER THE OTHER AND WHY?
         }
     }
 }
