de4b183ea85e1c87a24e1f5282c58fcf6b837804
AlexHorton1995 <alex.d.horton95@gmail.com>
2021-03-12 15:08:13 -0600

    Added PersonModel class.

---
+++ PersonAPI/Models/PersonModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+
+namespace PersonAPI.Models
+{
+    [DataContract]
+    public class PersonModel
+    {
+        [DataMember(Name = "ID")]
+        public string ID { get; set; }
+
+        [DataMember(Name = "firstName")]
+        public string FirstName { get; set; }
+
+        [DataMember(Name = "midInit")]
+        public string MidInit { get; set; }
+
+        [DataMember(Name = "lastName")]
+        public string LastName { get; set; }
+
+        [DataMember(Name = "dob")]
+        public DateTime DOB { get; set; }
+
+        [DataMember(Name = "age")]
+        public byte Age { get; set; }
+
+        [DataMember(Name = "address1")]
+        public string Address1 { get; set; }
+
+        [DataMember(Name = "address2")]
+        public string Address2 { get; set; }
+
+        [DataMember(Name = "city")]
+        public string City { get; set; }
+
+        [DataMember(Name = "state")]
+        public string State { get; set; }
+
+        [DataMember(Name = "zipCode")]
+        public string ZipCode { get; set; }
+
+        [DataMember(Name = "phone1")]
+        public string Phone1 { get; set; }
+
+        [DataMember(Name = "phone2")]
+        public string Phone2 { get; set; }
+
+        [DataMember(Name = "eMail")]
+        public string EMail { get; set; }
+
+    }
+}
