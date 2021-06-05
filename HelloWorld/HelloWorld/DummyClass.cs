40b0b1df96ee02d236f68d98e3b53f61ce727869
Alex Horton <alex.d.horton95@gmail.com>
2021-06-05 17:25:40 -0500

    Finished the first Excel Method.

--- HelloWorld/HelloWorld/DummyClass.cs
+++ HelloWorld/HelloWorld/DummyClass.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using HelloWorld.Models;
 using OfficeOpenXml;
 using System.IO;
+using System.Collections.Generic;
 
 [assembly: InternalsVisibleTo("HelloWorldTests")]
 namespace HelloWorld
@@ -41,7 +42,7 @@ namespace HelloWorld
             bool retType;
             try
             {
-                
+
                 var conSelection = args[0].Split(':').ToList();
                 retType = (DAO = dao.InitializeDAO(conSelection[1])) != null;
             }
@@ -102,23 +103,60 @@ namespace HelloWorld
             return recCount;
         }
 
-        internal static void DisplayInExcel(IOrderedEnumerable<QueryModel> items) 
+        internal static void DisplayInExcel(IOrderedEnumerable<QueryModel> items)
         {
-            //this has to be here for the excel license
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
-            using (var package = new ExcelPackage(new FileInfo(@"B:\OneDrive\Documents\MyWorkbook.xlsx")))
+            try
             {
 
-                var sheet1 = package.Workbook.Worksheets.Add("Sheet1");
+                //this has to be here for the excel license
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-                //var sheet1 = package.Workbook.Worksheets["Sheet1"];
-                sheet1.Cells[1, 1].Value = "dis some sheet here";
-                sheet1.Cells[1, 1].Style.Font.Bold = true;
+                using (var package = new ExcelPackage(new FileInfo(string.Format(@"B:\OneDrive\Documents\MyWorkbook_{0}.xlsx", DateTime.Now.ToString("MMddyyyy_HHmmss")))))
+                {
+                    var sheet1 = package.Workbook.Worksheets.Add("Sheet1");
 
-                package.Save();
+                    //add header row first
+                    HashSet<string> header = new HashSet<string>()
+                    {
+                        "InvoiceLineID","MediaTrackID","AlbumTitle","ArtistsName","TrackName",
+                        "MediaType","UnitPrice","Quantity"
+                    };
+
+                    int colInt = 1;
+                    int rowInt = 1;
+
+                    foreach(var cell in header)
+                    {
+                        sheet1.Cells[1, colInt].Value = cell;
+                        sheet1.Cells[1, colInt].Style.Font.Bold = true;
+                        ++colInt;
+                    }
+
+                    //reset the column to the first column
+                    colInt = 1;
+                    rowInt = 2;
+
+                    foreach (var item in items)
+                    {
+                        sheet1.Cells[rowInt, 1].Value = item.InvoiceLineID;
+                        sheet1.Cells[rowInt, 2].Value = item.MediaTrackID;
+                        sheet1.Cells[rowInt, 3].Value = item.AlbumTitle;
+                        sheet1.Cells[rowInt, 4].Value = item.ArtistsName;
+                        sheet1.Cells[rowInt, 5].Value = item.TrackName;
+                        sheet1.Cells[rowInt, 6].Value = item.MediaType;
+                        sheet1.Cells[rowInt, 7].Value = item.UnitPrice;
+                        sheet1.Cells[rowInt, 8].Value = item.Quantity;
+
+                        ++rowInt;
+                    }
+
+                    package.Save();
+                }
             }
-
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
         }
 
         #region Disposable
