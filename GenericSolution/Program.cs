6d133314d84e971ae8813bdd081ad09552d51f7a
Alex Horton <alex.d.horton95@gmail.com>
2021-07-03 22:57:47 -0500

    Adding new solutions

---
+++ GenericSolution/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GenericSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            /*
+             *         Dim someDecimal As Decimal = 100.1D
+        Dim stringNum As String = Nothing
+
+        someDecimal += Convert.ToDecimal(stringNum)
+             */
+
+            Decimal someDecimal = 100.00M;
+            string stringNum = null;
+
+            someDecimal += Convert.ToDecimal(stringNum);
+
+
+            sw.Stop();
+
+            Console.WriteLine("Number of millisecs to run: " + sw.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
