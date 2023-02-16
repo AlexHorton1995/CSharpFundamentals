8a40f6245eccad97233565c5951a168f8da63ca2
Alex Horton <alex.d.horton@hotmail.com>
2023-02-15 18:08:44 -0600

    Learning C++ for myself :)

---
+++ LessonFive/LessonFive/LessonFive.cpp
@@ -0,0 +1,14 @@
+// LessonFive.cpp : Working with Numbers
+//
+
+//video playlist link https://youtube.com/playlist?list=PLLAZ4kZ9dFpOSzRXG05goZMgsvXwDDL6g
+//lesson 9
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+}
+
