8ca2906f546dade24fbc962822491ff4ee881147
Alex Horton <alex.d.horton@hotmail.com>
2023-02-16 20:23:40 -0600

    committing to master

--- LessonFive/LessonFive/LessonFive.cpp
+++ LessonFive/LessonFive/LessonFive.cpp
@@ -9,6 +9,27 @@ using namespace std;
 
 int main()
 {
-	
+	//Whole numbers = ints
+	//floats, doubles
+	//unsigned numbers as well
+
+	//math operations
+	// Addition +
+	// Subtraction -
+	// Multiplication *
+	// Division /
+	// Modulus % - gets the remainder of dividing two numbers
+
+	int numberA = 10;
+	int numberB = 3;
+	int numberC = 15;
+	long total = 0;
+	long totalMod = 0;
+	total = (numberA + numberB) * numberC;
+	totalMod = numberA % numberB;
+
+	cout << "Sum: " << total << endl; //should be 13
+	cout << "Mod result of A and B: " << totalMod++ << endl;
+	return 0;
 }
 
